8b6517f8615517a9da4daa2a180394d2acb9bd64
Rahulkt2000 <rahulkt291@gmail.com>
2024-11-28 06:02:33 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,30 @@
+function sayMyName(){
+    console.log("H");
+    console.log("i")
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H")
+
+}
+
+//sayMyName()
+
+//function addTwoNumbers(number1,number2){
+    //console.log(number1 + number2);
+//}
+function addTwoNumbers(number1 , number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+
+}
+
+ const result = addTwoNumbers(7,5)
+ //console.log("Result:", result);   
+
+ function loginUserMessage(username){
+    return `${username} just logged in`
+ }
+  console.log(loginUserMessage("Rahul"));
