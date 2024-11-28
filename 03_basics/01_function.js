50e901436b876dfab5bb4fd719d50eb2519c7281
Rahulkt2000 <rahulkt291@gmail.com>
2024-11-28 06:50:26 +0000

    01_function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -27,4 +27,36 @@ function addTwoNumbers(number1 , number2){
  function loginUserMessage(username){
     return `${username} just logged in`
  }
-  console.log(loginUserMessage("Rahul"));
+  //console.log(loginUserMessage("Rahul"));
+
+  function calculatecartprice(val1,val2,...num1){ // rest operator 
+    return num1
+  }
+
+  //console.log(calculatecartprice(200,300,400));
+
+  const user = {
+    username: "Rahul",
+    price: 199
+  }
+
+  function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+  }
+
+  //handleObject(user)
+
+  handleObject ({
+    username: "sam",
+    price: 499
+  })
+
+
+  const newArray = [200,300,400,500,800]
+
+  function returnSecondValue(getArray){
+    return getArray[2]
+  }
+  //console.log(returnSecondValue(newArray))
+  console.log(returnSecondValue([200,400,500,800,700]));
